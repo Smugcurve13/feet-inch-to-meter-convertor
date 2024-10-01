da9d914ce21562732d29cd21a204dc35c3e73f2d
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-01 13:38:48 +0530

    added black theme and implemented exit button

--- app.py
+++ app.py
@@ -3,18 +3,22 @@
 import FreeSimpleGUI as gui
 from convertor import convert
 
+gui.theme="Black"
+
 feet_label = gui.Text("Enter Feet: ")
 feet_input = gui.Input(key = 'feets')
+
 inch_label = gui.Text("Enter Inches: ")
 inch_input = gui.Input(key = 'inches')
 
-convert_button = gui.Button("Convert")
+convert_button = gui.Button("Convert", key="convert")
+exit_button = gui.Button("Exit",key='exit')
 output_label = gui.Text(key = "output",text_color="white",)
 
 window = gui.Window("Convertor",
                     layout = [[feet_label,feet_input],
                               [inch_label,inch_input],
-                              [convert_button, output_label]],
+                              [convert_button, exit_button ,output_label]],
                     font = (15))
 
 while True:
@@ -23,7 +27,11 @@ while True:
     feet = values["feets"]
     inch = values["inches"]
     meters = convert(feet,inch)
-    window["output"].update(value = f"{round(meters,2)}m")
+    match event:
+        case 'convert':
+            window["output"].update(value = f"{round(meters,3)}m")
+        case 'exit':
+            break
 
 window.read()
 window.close()
